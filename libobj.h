7c6a31d1bc0a32d749629fd6a8a892753acc45aa
Azva <66280758+Azva@users.noreply.github.com>
2020-06-05 17:38:37 +0200

    Add files via upload
    adding code

---
+++ libobj.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef LIBOBJ_H
+#define LIBOBJ_H
+
+float obj(float bok)
+{
+    return bok*bok*bok;
+}
+
+#endif
---
+++ libpole.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef LIBPOLE_H
+#define LIBPOLE_H
+
+float polePow(float bok)
+{
+    return bok*bok;
+}
+
+#endif
